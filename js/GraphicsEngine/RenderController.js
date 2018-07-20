547061bec4dd99763bfbfa58ea94cb06be32e8d4
Andru <kaz.19.01.1999@yandex.ru>
2018-07-21 01:29:41 +1000

    Исправления RenderController

--- js/GraphicsEngine/RenderController.js
+++ js/GraphicsEngine/RenderController.js
@@ -1,12 +1,11 @@
 class RenderController {
-    constructor(arr)
-    {
-        this.BaseSpritesArr = arr;
+    constructor(arr, canvas) {
+        this.sprites = arr;
+        this.canvas = canvas;
     }
 
-    renderSprite(type, variant, canvas, x, y){
-        (this.sprites[type][(Math.abs(variant)%this.sprites[type].length)]).draw(canvas,x,y); // Символ дибилизма
+    renderSprite(type, variant, x, y) {
+        this.sprites[type][Math.abs(variant) % this.sprites[type].length].draw(this.canvas, x, y); // Символ дибилизма
         //Отрисовка справйта
     }
-
 }
