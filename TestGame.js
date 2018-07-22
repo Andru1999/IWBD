b694ce2b39171b857bcf12d0918d603a79de13e7
Alkonior <sashadz2001@mail.ru>
2018-07-22 19:27:38 +1000

    Add DisplayControler(Drag&Drop and Mouse Clicks detection)

--- TestGame.js
+++ TestGame.js
@@ -17,13 +17,15 @@ window.onload=()=>
 };
 
 class Game {
-    init(SpriteArr,canvas)
+    init(SpriteArr,maincanvas)
     {
-        canvas.width=1400;
-        canvas.height=800;
-
+        maincanvas.width=1400;
+        maincanvas.height=800;
         this.engine = new SpaceWorld();
-        this.renderManager=new RenderManager(SpriteArr,canvas,this.engine,this.engine.getWorldSize());
+        this.renderManager=new RenderManager(SpriteArr,maincanvas,this.engine,this.engine.getWorldSize());
+        $(maincanvas).mousedown(game.renderManager.DisplControler.mousedown);
+        $("body").mouseup(game.renderManager.DisplControler.mouseup);
+        $("body").mousemove(game.renderManager.DisplControler.mousemove);
     }
 
 
